62f90d551346709449403311c8ba4edbb9bc56b9
kashookashif5 <kashookashif5@gmail.com>
2024-06-05 21:22:06 +0500

    first commit

---
+++ months.js
@@ -0,0 +1,29 @@
+let data =prompt("eneter data")
+
+switch(data){
+    case(data="1"): console.log("jan")
+    break;
+    case(data="2"):console.log("feb")
+    break;
+    case(data="3"): console.log("march")
+    break;
+    case(data="4"): console.log("apl")
+    break;
+    case(data="5"): console.log("may")  
+    break;
+    case(data="6"): console.log("jun")
+    break;
+    case(data="7"): console.log("july")
+    break;
+    case(data="8"): console.log("agst")
+    break;
+    case(data="9") :console.log("sept")
+    break;
+    case(data="10"): console.log("oct")
+    break;
+    case(data="11"): console.log("nov")
+    break;
+    case(data="12"): console.log("dec")
+    break;
+    defaul(errer)
+}
